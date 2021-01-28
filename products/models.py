dfc8209e001b0b57f10ea724e367731223b4effb
KrisK1978 <k_kempa@yahoo.co.uk>
2021-01-28 10:38:30 +0000

    Restyled and customized admin

--- products/models.py
+++ products/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 # Guitar category model.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-# String method which takes in the category model itself.
+# Return string method which takes in the category model itself.
     def __str__(self):
         return self.name
 
