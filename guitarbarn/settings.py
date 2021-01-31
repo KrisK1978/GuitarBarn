084497f373e7e08a074af9aca05d1f185c687454
KrisK1978 <k_kempa@yahoo.co.uk>
2021-01-31 23:47:10 +0000

    Removed secret key and set debug

--- guitarbarn/settings.py
+++ guitarbarn/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ntn+d7deubjmdbcsuh^=0i5g@e-ytb5c5=qw4gz*9qo$_nn3kv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['guitarbarn.herokuapp.com', 'localhost']
 
